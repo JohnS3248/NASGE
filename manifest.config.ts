921a14e017d641d1c86cbdd85d2040792ca23200
JohnS3248 <JohnS3248@outlook.com>
2026-01-02 18:09:34 +0800

    chore: 添加扩展图标并升级至 v0.9.0
    - 添加 NASGE logo 图标 (16/48/128px)
    - manifest 配置扩展图标和工具栏图标
    - 版本号升级至 0.9.0
    - .gitignore 添加 zips/ 打包输出目录

--- manifest.config.ts
+++ manifest.config.ts
@@ -6,8 +6,18 @@ export default defineManifest({
   name: "NASGE",
   version: VERSION,
   description: "创建者友好的 Steam 指南编辑浏览器扩展",
+  icons: {
+    "16": "icon/icon-16.png",
+    "48": "icon/icon-48.png",
+    "128": "icon/icon-128.png"
+  },
   action: {
-    default_popup: "src/popup/index.html"
+    default_popup: "src/popup/index.html",
+    default_icon: {
+      "16": "icon/icon-16.png",
+      "48": "icon/icon-48.png",
+      "128": "icon/icon-128.png"
+    }
   },
   background: {
     service_worker: "src/background/index.ts",
